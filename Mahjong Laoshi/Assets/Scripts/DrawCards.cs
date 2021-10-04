2c24f5ce510db7568b848451d59f2d15be63c218
s-brumleyma <s-brumleyma@bsd405.org>
2021-10-04 13:08:55 -0700

    Restored Old Version, New Script
    restored old assets, organized into folders, started writing new DealCards script on a button

---
+++ Mahjong Laoshi/Assets/Scripts/DrawCards.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+    public GameObject OneDotTile;
+    public GameObject EightBambooTile;
+    public GameObject PlayerArea;
+    public GameObject OpponentArea;
+    public GameObject DealArea;
+
+    void Start()
+    {
+        
+    }
+}
