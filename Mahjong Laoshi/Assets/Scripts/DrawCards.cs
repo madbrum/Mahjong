10c36c60072e3890b2970956dfa53b9090ada5bd
s-brumleyma <s-brumleyma@bsd405.org>
2021-10-14 13:06:48 -0700

    Loading Tile Sprites Into List
    to deal a full set of tiles

--- Mahjong Laoshi/Assets/Scripts/DrawCards.cs
+++ Mahjong Laoshi/Assets/Scripts/DrawCards.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrawCards : MonoBehaviour
 {
@@ -11,10 +12,25 @@ public class DrawCards : MonoBehaviour
     public GameObject DealArea;
     public Sprite DotSprite;
 
+    private List<GameObject> t = new List<GameObject>();
+    private Object[] TileSprites;
+
     List<GameObject> tiles = new List<GameObject>();
 
     void Start()
     {
+        //KEY THING is that you can't just randomize these tiles! there are only a set number of dot, bam, ect tiles that exist! no dupes beyond a certain point! maybe a counter?  
+        //eventually you will also need to tag these with type identities and probably make a new tile array but o well
+        TileSprites = Resources.LoadAll("Tile Sprites");
+        for (int i = 0; i < TileSprites.Length; i++)
+        {
+            //create a Tile GameObject, load it with the current tile sprite
+            //for int i = 0 i < 4, add a new "instance" (not actually in the map, just... existing somewhere, like any other variable) of this object to tiles 
+            //basically you want to load this array with copies of overloaded prefabs, remove each upon actual instantiation...
+            //so if i do something like List.add(dot1) four times... what happens? 
+            //in essence this is you building your SET of tiles, which are then dealt out at random
+        }
+
         tiles.Add(Dot1);
         tiles.Add(Bam8);
     }
@@ -24,9 +40,11 @@ public class DrawCards : MonoBehaviour
         for (int i = 0; i < 13; i++)
         {
             GameObject playerTile = Instantiate(tiles[Random.Range(0, tiles.Count)], new Vector3(0, 0, 0), Quaternion.identity);
-            //playerTile.getComponent("Image").image = DotSprite;
+            //Resources.loadAll
+            playerTile.GetComponent<Image>().sprite = DotSprite;
             playerTile.transform.SetParent(PlayerArea.transform, false);
 
+            
             GameObject opponentTile = Instantiate(tiles[Random.Range(0, tiles.Count)], new Vector3(0, 0, 0), Quaternion.identity);
             opponentTile.transform.SetParent(OpponentArea.transform, false);
         }
